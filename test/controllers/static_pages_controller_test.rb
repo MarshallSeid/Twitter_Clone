436efef45290e2056c7607cf7506bef6ba86f7ad
Marshall Seid <mseid@ucsd.edu>
2015-07-02 04:33:50 +0000

    Finished layout/routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,26 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home #tests home page 
     assert_response :success
-    assert_select "title", "Marshall Twitter Home"
+    assert_select "title", "Mwitter | Home"
   end
-  test "should get tim" do 
-	get :tim
-	assert_response :success
-	assert_select "title", "Marshall Twitter Tim"
-  end
+
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Marshall Twitter Help"
+    assert_select "title", "Mwitter | Help"
   end
 
   test "should get about" do 
 	get :about
 	assert_response :success
-	assert_select "title", "Marshall Twitter About"
+	assert_select "title", "Mwitter | About"
   end
 
+  test "should get contact" do 
+	get :contact
+	assert_response :success
+	assert_select "title", "Mwitter | Contact"
+  end
   #Tests for slightly dynamic static pages
   
 end

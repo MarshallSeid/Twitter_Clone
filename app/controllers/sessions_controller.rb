71ad353272d693d3606bd5c0a63a9f4c25097c0d
Marshall Seid <mseid@ucsd.edu>
2015-07-11 09:06:26 +0000

    Finish user edit, update, index and destroY

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      # ? is equal to if(params..){ (remember)} else forget
+      # ? is equal to if(params..) (remember) else forget
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false} 
   #validates_length_of :name, :minimum => 5
-  validates :password, presence: true, length: {minimum: 6}
   has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   
   # Returns the hash digest of the given string
   def User.digest(string)

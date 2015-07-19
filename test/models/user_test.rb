8c6549e0c8ed1dfd72e68f3636c9c5bc733ccdd2
Marshall Seid <mseid@ucsd.edu>
2015-07-19 14:57:19 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -79,4 +79,36 @@ class UserTest < ActiveSupport::TestCase
            @user.destroy
         end
     end
+    
+    test "should follow and unfollow a user" do
+      marshall = users(:marshall)
+      archer = users(:archer)
+      assert_not marshall.following?(archer)
+      marshall.follow(archer)
+      assert marshall.following?(archer)
+      assert archer.followers.include?(marshall)
+      marshall.unfollow(archer)
+      assert_not marshall.following?(archer)
+    end
+    
+    test "feed should have the right posts" do
+      marshall = users(:marshall)
+      archer = users(:archer)
+      lana = users(:lana)
+      
+      # Posts from followed user
+      lana.microposts.each do |post_following|
+        assert marshall.feed.include?(post_following)
+      end
+      
+      # Posts from self
+      marshall.microposts.each do |post_self|
+        assert marshall.feed.include?(post_self)
+      end
+      
+      # Posts from unfollowed user
+      archer.microposts.each do |post_unfollowed|
+        assert_not marshall.feed.include?(post_unfollowed)
+      end
+    end
 end
